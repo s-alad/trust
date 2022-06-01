b18fd72fa3617ae1e25b3db0cbb8a6303ca5c260
s-alad <n7.saad@gmail.com>
2022-06-01 15:16:34 -0400

    email signin

--- trust/lib/screens/authenticate/sign_in.dart
+++ trust/lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:trust/screens/auth.dart';
+import 'package:trust/services/auth.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -10,6 +10,9 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+
+  String email = '';
+  String password = '';
 
   signIn() async {
     dynamic result = await _auth.signInAnon();
@@ -27,11 +30,40 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: ElevatedButton(
-          child: const Text('Proceed Without Account'),
-          onPressed: () async {
-            signIn();
-          },
+        child: Column(
+          children: <Widget>[
+            Form(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    onChanged: (value) => {setState(() => email = value)},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    onChanged: (value) => {setState(() => password = value)},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () => {print(email + " " + password)},
+                      child: const Text("sign in with email and password"))
+                ],
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Proceed Without Account'),
+              onPressed: () async {
+                signIn();
+              },
+            ),
+          ],
         ),
       ),
     );
