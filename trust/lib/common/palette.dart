8192a4e38b215c34925d192babe2528ca027e8e5
s-alad <n7.saad@gmail.com>
2022-06-01 00:25:28 -0400

    palette

---
+++ trust/lib/common/palette.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Palette {
+  final primary = const Color(0xFF161412);
+  final secondary = const Color(0xFF5A473A);
+  final tertiary = const Color(0xFFC7BDB1);
+  final accent = const Color(0xFFDDD9D6);
+}
